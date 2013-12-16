72d289e130bcf6daba141d0cd98b585e679c0605
DrSithis <hoflack.david@hotmail.fr>
2013-12-16 10:22:08 +0100

    core define

--- core/core.php
+++ core/core.php
@@ -5,8 +5,18 @@ session_start();
 define('ROOT_PATH', dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
 
 define('CORE', ROOT_PATH . 'core/');
-define('VUE', ROOT_PATH . 'assets/');
+    define('DB', CORE . 'bdd/');
+    define('FUNC', CORE . 'function/');
+    define('CLASSE', CORE . 'class/');
+    define('MANAGE', CORE . 'manager/');
+
+define('RESS', ROOT_PATH . 'assets/');
 define('VIEW', ROOT_PATH . 'view/');
-define('INC', CORE . 'include/');
-define('DB', CORE . 'db/');
-define('FUNC', CORE . 'function/');
+
+
+
+
+require_once(DB . '/connect_pdo.php');
+
+
+
